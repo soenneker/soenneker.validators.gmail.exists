759a301d9775c01c86c42212bfbdd729256b2afd
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:52 -0500

    xml commenting

--- src/Soenneker.Validators.Gmail.Exists/GmailExistsValidator.cs
+++ src/Soenneker.Validators.Gmail.Exists/GmailExistsValidator.cs
@@ -84,11 +84,18 @@ public sealed class GmailExistsValidator : Validator.Validator, IGmailExistsVali
         return false;
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(GmailExistsValidator));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(GmailExistsValidator));
